e87a346f3c30230a947bf575ef2c7181a2dbd78f
marcovins <marcosbelods@gmail.com>
2024-06-28 16:54:59 -0300

    Modelagem da classe principal

--- Projeto_BancoJava/src/com/banco/Banco.java
+++ Projeto_BancoJava/src/com/banco/Banco.java
@@ -6,18 +6,21 @@ import java.util.Set;
 import com.banco.Cliente.Cliente;
 
 public class Banco {
-
+    // Conjunto de clientes do banco
     Set<Cliente> clientes;
 
-    Banco() {
+    // Construtor da classe Banco, inicializa o conjunto de clientes
+    public Banco() {
         this.clientes = new HashSet<Cliente>();
     }
 
+    // Adiciona um cliente ao conjunto de clientes
     public void adicionarCliente(Cliente cliente) {
         this.clientes.add(cliente);
     }
 
-    public Cliente buscarCliente(Cliente clienteBuscar ) {
+    // Busca um cliente no conjunto de clientes, retornando o cliente se encontrado, caso contrário, retorna null
+    public Cliente buscarCliente(Cliente clienteBuscar) {
         for (Cliente cliente : this.clientes) {
             if (cliente.equals(clienteBuscar)) {
                 return cliente;
@@ -26,11 +29,12 @@ public class Banco {
         return null;
     }
 
+    // Remove um cliente do conjunto de clientes, se o cliente for encontrado
     public void removerCliente(Cliente cliente) {
-        if(buscarCliente(cliente).equals(cliente)){
+        // Verifica se o cliente existe no conjunto antes de removê-lo
+        Cliente clienteEncontrado = buscarCliente(cliente);
+        if (clienteEncontrado != null && clienteEncontrado.equals(cliente)) {
             this.clientes.remove(cliente);
         }
     }
-     
-    
 }
